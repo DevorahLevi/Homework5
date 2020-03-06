1dbe995924e99366773cf019d4d9448699be8c31
Devorah Levi <devorahlevi1@gmail.com>
2020-03-05 20:36:11 -0600

    *

--- src/main/java/edu/blitstein/calc/CalculatorApp.java
+++ src/main/java/edu/blitstein/calc/CalculatorApp.java
@@ -4,6 +4,7 @@ import edu.blitstein.calc.engine.op.engine.Calculator;
 import edu.blitstein.calc.exception.DivideByZeroException;
 import edu.blitstein.calc.exception.UnknownOpException;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.InputMismatchException;
@@ -23,25 +24,30 @@ public class CalculatorApp {
         Calculator clerk = new Calculator();
         System.out.println("Starting with = " + clerk.getResult());
 
-        String fileName = "src/main/resources/" + args[0];
+        String outputFileName = "src/main/resources/" + args[0];
         PrintWriter printWriter = null;
 
-        boolean done = false;
+        String inputFileName = "src/main/resources/" + args[1];
+        Scanner inputStream = null;
+
         try {
-            printWriter = new PrintWriter(fileName);
+            printWriter = new PrintWriter(outputFileName);
             printWriter.println("Calculator is on and set to 0.0");
-            while (!done) {
-                String opType = (keyboard.next()); //****
+
+            File file = new File(inputFileName);
+            inputStream = new Scanner(file);
+
+            while (inputStream.hasNextLine()) {
+                String opType = (inputStream.next()); //****
                 if (opType.substring(0,1).equalsIgnoreCase("E"))
-                    done = true;
+                    break;
                 else {
                     try {
-                        double nextNumber = keyboard.nextDouble();
+                        double nextNumber = inputStream.nextDouble();
                         double result = clerk.evaluate(opType, nextNumber);
                         printWriter.println("result " + opType + " " + nextNumber + " = " + result);
                         printWriter.println("updated result = " + result);
                         } catch (DivideByZeroException e) {
-                            //e.printStackTrace(); **used for finding where the error is
                             System.out.println(e.getMessage());
                         } catch (UnknownOpException e) {
                             //e.printStackTrace();
@@ -56,10 +62,13 @@ public class CalculatorApp {
             //System.out.println("The final result is " + clerk.getResult()); //***How does this line get the updated result from clerk? We x update it anywhere
             printWriter.println("Calculator program ending.");
         } catch (FileNotFoundException e) {
-            System.out.println("Error opening the file " + fileName + ": " + e.getMessage());
+            System.out.println("Error opening the file " + outputFileName + ": " + e.getMessage());
         } finally {
             if (printWriter != null) {
                 printWriter.close();
+            }
+            if (inputStream != null) {
+                inputStream.close();
             }
         }
     }
